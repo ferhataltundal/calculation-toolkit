a6dcc211eabea6de353b650c973308069556b975
Ferhat Altundal <100750736+ferhataltundal1@users.noreply.github.com>
2024-01-31 16:59:48 +0300

    Version 1.0.1

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-const { tempCalc, lengthCalc } = require("calculation-toolkit");
+import { tempCalc, lengthCalc } from "calculation-toolkit";
 
 console.log(tempCalc("35 kelvin to rankine", "result"));
 console.log(lengthCalc("60 cm to yard", "result"));
